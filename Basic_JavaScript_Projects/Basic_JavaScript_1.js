de3965213a29871f05d3c2417e85c2448cc37bf8
jonreesecooper <70401154+jonreesecooper@users.noreply.github.com>
2020-09-09 13:24:29 -0400

    Add files via upload

---
+++ Basic_JavaScript_Projects/Basic_JavaScript_1.js
@@ -0,0 +1,24 @@
+var A = "Hello, world!"; //This sets a variable with the string "Hello World"
+
+window.alert(A); //This creates a pop up window holding the value for variable A
+
+var B = "  Get" + " Money"; //This is a concatenation of two strings
+var B = B.fontcolor("red"); //this assigns a font color the variable
+
+document.write(B); //this prints the value of variable B as text on the page
+
+var Friends = " Good People", Orange = " Adam", Yellow = " Charley", Pink = " Dave", Purple = " Omar", Green = " Ryan  "; //this is assigning multiple values
+var Friends = Friends.fontcolor("blue");
+var Orange = Orange.fontcolor("orange");
+var Yellow = Yellow.fontcolor("yellow");
+var Pink = Pink.fontcolor("pink");
+var Purple = Purple.fontcolor("purple");
+var Green = Green.fontcolor("green")
+document.write(Friends);
+document.write(Orange);
+document.write(Yellow);
+document.write(Pink);
+document.write(Purple);
+document.write(Green);
+document.write(5*4);
+
